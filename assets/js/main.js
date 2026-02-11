2d700b5ff2e645e82c1d9f37a876d920576b6ad0
yannis-cloud <yannis.albert78@gmail.com>
2026-02-11 09:19:20 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Scroll Reveal Eldora style
+// ===== Reveal animation =====
 const observer = new IntersectionObserver((entries)=>{
 entries.forEach(entry=>{
 if(entry.isIntersecting){
@@ -9,4 +9,30 @@ entry.target.classList.add("show");
 
 document.querySelectorAll(".reveal").forEach(el=>{
 observer.observe(el);
+});
+
+
+// ===== Navbar highlight au scroll =====
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".menu a");
+
+window.addEventListener("scroll",()=>{
+let current="";
+
+sections.forEach(sec=>{
+const top = window.scrollY;
+const offset = sec.offsetTop - 200;
+const height = sec.offsetHeight;
+
+if(top >= offset && top < offset + height){
+current = sec.getAttribute("id");
+}
+});
+
+navLinks.forEach(a=>{
+a.classList.remove("active");
+if(a.getAttribute("href")==="#"+current){
+a.classList.add("active");
+}
+});
 });
