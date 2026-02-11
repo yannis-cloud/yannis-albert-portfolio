c4c470c9325ac7994ad296e4ece8750c72b6b089
yannis-cloud <yannis.albert78@gmail.com>
2026-02-11 09:08:03 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,15 @@ if (revealEls.length) {
 
   revealEls.forEach((el) => io.observe(el));
 }
+// Scroll Reveal Eldora style
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+},{threshold:0.15});
+
+document.querySelectorAll(".reveal").forEach(el=>{
+observer.observe(el);
+});
